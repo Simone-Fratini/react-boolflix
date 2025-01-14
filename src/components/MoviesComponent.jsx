410a1246bb7c5821301305beda784e2b6e8861b2
Simone-Fratini <simone@fratini.com>
2025-01-14 22:35:11 +0100

    Fix typo in MoviesComponent function name and export

--- src/components/MoviesComponent.jsx
+++ src/components/MoviesComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CardsComponent from './CardsComponent'
 
-function Moviesomponent() {
+function MoviesComponent() {
   return (
     <section>
       <h2 className='font-bold text-white text-3xl mb-5'>Movies</h2>
@@ -11,4 +11,4 @@ function Moviesomponent() {
   )
 }
 
-export default Moviesomponent
+export default MoviesComponent
