808751d20d95fd91add371b87bfb9524b6da546e
Simone-Fratini <simone@fratini.com>
2025-01-14 13:09:05 +0100

    Refactor HeaderComponent layout for improved responsiveness and styling

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -13,12 +13,12 @@ function HeaderComponent() {
 
     return (
         <header className='bg-black text-white'>
-            <div className='container mx-auto p-4 flex items-center justify-between'>
+            <div className='container mx-auto p-4 flex flex-col items-center md:flex-row md:items-center md:justify-between'>
                 {/* Titolo */}
                 <h1 className='text-4xl font-bold text-red-600'>BOOLFLIX</h1>
                 
                 {/* Form */}
-                <form onSubmit={handleSubmit} className='flex items-center gap-4'>
+                <form onSubmit={handleSubmit} className='flex items-center flex-col gap-4 mt-4 md:mt-0 md:flex-row'>
                     {/* Generi tendina */}
                     <select name="genres" id="genres" className='p-1 rounded bg-gray-800 text-white border border-gray-600' defaultValue="all" >
                         <option value="all">Tutti i generi</option>
@@ -26,14 +26,15 @@ function HeaderComponent() {
                         <option value="azione">Azione</option>
                         <option value="horror">Horror</option>
                     </select>
-                    
-                    {/* search */}
-                    <input type='text' name="search" className='p-1 rounded bg-gray-800 text-white border border-gray-600' placeholder='Search' />
+                    <div className='flex items-center gap-2'>
+                        {/* search */}
+                        <input type='text' name="search" className='p-1 rounded bg-gray-800 text-white border border-gray-600' placeholder='Search' />
 
-                    {/* submit */}
-                    <button type="submit" className='p-1 pr-2 pl-2 rounded bg-red-600 text-white font-bold hover:bg-red-700' >
-                        Cerca
-                    </button>
+                        {/* submit */}
+                        <button type="submit" className='p-1 pr-2 pl-2 rounded bg-red-600 text-white font-bold hover:bg-red-700' >
+                            Cerca
+                        </button>
+                    </div>
                 </form>
             </div>
         </header>
