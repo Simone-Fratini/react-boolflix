9b1e94267a04d5008e840506e178a82282a3e959
Simone-Fratini <simone@fratini.com>
2025-01-14 13:45:41 +0100

    Update MainComponent layout and add MovieComponent for media display

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function MainComponent() {
   return (
-    <main className='container mx-auto p-4'>
-        <div>Ciao</div>
+    <main className='container mx-auto p-4 mt-11'>
+        <h2 className='font-bold text-white text-3xl'>Movies</h2>
     </main>
   )
 }
---
+++ src/components/MovieComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MovieComponent() {
+  return (
+    <div>MovieComponent</div>
+  )
+}
+
+export default MovieComponent
