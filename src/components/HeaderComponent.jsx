3aa5f6ed5403a6f2522127f1d2e8f45110c05713
Simone-Fratini <simone@fratini.com>
2025-01-14 12:48:39 +0100

    Enhance HeaderComponent with styled header and search input

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function HeaderComponent() {
   return (
-    <div>HeaderComponent</div>
+    <header className='bg-black'>
+        <div className='container mx-auto p-4 flex items-center justify-between'>
+            <h1 className='text-4xl font-bold text-red-600'>BOOLFLIX</h1>
+            <div>
+                
+                <input type='text' className='' placeholder='Search' />
+            </div>
+        </div>
+    </header>
   )
 }
 
